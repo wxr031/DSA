668765e2afc29486430a0e944e9ac80d9e011b8c
wxr031 <wxr031@gmail.com>
2017-11-20 23:06:39 +0800

    add new

--- linked_list/find_node_from_the_end.c
+++ linked_list/find_node_from_the_end.c
@@ -7,23 +7,20 @@ typedef struct listnode {
 	struct listnode *next;
 } Listnode;
 // First Version: Build a hash table:
-// If head is NULL, return INT_MIN
 // Time complexity: O(n), since we need to traverse the whole linked list. 
 // Space complexity: O(n), since we need to build a hash table of size n.
 int find_from_end1(Listnode *head, int pos) {
 	int hash[MAXN] = {}, N = 0;
-	if (head == NULL) return INT_MIN;
 	Listnode *curr = head;
 	while (curr != NULL) {
 		hash[N ++] = curr->data;
 		curr = curr->next;
 	}
-	return hash[N - pos];
+	return hash[N - pos - 1];
 }
-//Second Version: Find N (total number of linked list), the find n - pos - 2 one
-// If head is NULL, return INT_MIN
+//Second Version: Find N (total number of linked list), the find N - pos - 2 one
 // Time complexity: O(n)
-// Spaxe complexity: O(2)
+// Spaxe complexity: O(1)
 int find_from_end2(Listnode *head, int pos) {
 	int N = 0;
 	Listnode *curr = head;
@@ -31,11 +28,25 @@ int find_from_end2(Listnode *head, int pos) {
 		N ++;
 		curr = curr->next;
 	}
-	for (int i = 0; i < N - pos; i ++)
+	for (int i = 0; i < N - pos - 1; i ++)
 		head = head->next;
 	return head->data;
 }
+//Third Version: Single traversal
+// If head is NULL, return INT_MIN
+// Time complexity: O(n)
+// Spaxe complexity: O(1)
+int find_from_end3(Listnode *head, int pos) {
+	Listnode *temp = head, *curr = head;
+	for (int i = 0; i <= pos; i ++)
+		temp = temp->next;
+	while (temp != NULL) {
+		curr = curr->next;
+		temp = temp->next;
+	}
+	return curr->data;
+}
 int main() {
-	/*Add test data*/
+	/*Add test code*/
 	return 0;
 }
