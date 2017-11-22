d52cce314a687a77a412d9fe19918c8cccc06417
wxr031 <wxr031@gmail.com>
2017-11-22 11:09:05 +0800

    add

---
+++ linked_list/find_loop_start.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct listnode {
+	struct listnode *next;
+} Listnode;
+// Check if the given list contains loop or not.
+// If there is a loop in the list, find the start of the loop. Else, return NULL.
+// Time complexity: O(n)
+// Space complexity: O(1)
+Listnode *find_loop_start(Listnode *head) {
+	if (head == NULL || head->next == NULL)
+		return NULL;
+	Listnode *slow = head, *fast = head;
+	int has_loop = 0;
+	while (fast && !has_loop) {
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+			has_loop = 1;
+	}
+	if (has_loop) {
+		slow = head;
+		while (slow != fast) {
+			slow = slow->next;
+			fast = fast->next;
+		}
+		return slow;
+	}
+	else return NULL;
+}
+int main() {
+	/*add test code*/
+	Listnode *head = (Listnode *)malloc(sizeof(Listnode));
+	head->next = (Listnode *)malloc(sizeof(Listnode));
+	head->next->next = (Listnode *)malloc(sizeof(Listnode));
+	head->next->next->next = (Listnode *)malloc(sizeof(Listnode));
+	head->next->next->next->next = (Listnode *)malloc(sizeof(Listnode));;
+	head->next->next->next->next->next = (Listnode *)malloc(sizeof(Listnode));;
+	head->next->next->next->next->next->next = (Listnode *)malloc(sizeof(Listnode));;
+	head->next->next->next->next->next->next->next = (Listnode *)malloc(sizeof(Listnode));;
+	head->next->next->next->next->next->next->next->next = (Listnode *)malloc(sizeof(Listnode));;
+	head->next->next->next->next->next->next->next->next->next = (Listnode *)malloc(sizeof(Listnode));;
+	head->next->next->next->next->next->next->next->next->next->next = (Listnode *)malloc(sizeof(Listnode));;
+	head->next->next->next->next->next->next->next->next->next->next->next = head->next->next->next;
+	printf("%p =? %p\n", head->next->next->next, find_loop_start(head));
+	return 0;
+}
