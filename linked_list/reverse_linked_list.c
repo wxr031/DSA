ba70466e5a8867a09df855ba96d636cf750c567a
wxr031 <wxr031@gmail.com>
2017-11-22 21:21:42 +0800

    add array stack

--- linked_list/reverse_linked_list.c
+++ linked_list/reverse_linked_list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef struct listnode {
 	int data;
 	struct listnode *next;
@@ -6,7 +7,7 @@ typedef struct listnode {
 // Time complexity: O(n)
 // Space complexity: O(1)
 Listnode *reverse(Listnode *head) {
-	Listnode *prev = NULL, curr = head, *after = NULL;
+	Listnode *prev = NULL, *curr = head, *after = NULL;
 	while (curr != NULL) {
 		after = curr->next;
 		curr->next = prev;
@@ -17,5 +18,17 @@ Listnode *reverse(Listnode *head) {
 }
 int main() {
 	/* Add test code */
+	Listnode *head = (Listnode *)malloc(sizeof(Listnode));
+	head->data = 0;
+	head->next = (Listnode *)malloc(sizeof(Listnode));
+	head->next->data = 1;
+	head->next->next = (Listnode *)malloc(sizeof(Listnode));
+	head->next->next->data = 2;
+	head->next->next->next = (Listnode *)malloc(sizeof(Listnode));
+	head->next->next->next->data = 3;
+	head->next->next->next->next = NULL;
+	head = reverse(head);
+	for (Listnode *curr = head; curr != NULL; curr = curr->next)
+		printf("%d\n", curr->data);
 	return 0;
 }
