26bda3888bedaadbf77a05cf0cd96d28a908d327
wxr031 <wxr031@gmail.com>
2017-11-23 19:54:09 +0800

    add ll stack

---
+++ stack/linked_list_stack.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class LinkedListStack;
+
+class Listnode {
+private:
+	int data;
+	Listnode *next;
+public:
+	Listnode(): next(NULL) {}
+	Listnode(int n): data(n), next(NULL) {}
+
+	friend class LinkedListStack;
+};
+class LinkedListStack {
+private:
+	int top;
+	Listnode *head;
+public:
+	LinkedListStack(): top(-1) {}
+	bool is_empty();
+	void push(int);
+	int pop();
+	int get_size();
+};
+
+bool LinkedListStack::is_empty() {
+	return (top == -1);
+}
+void LinkedListStack::push(int n) {
+	Listnode *newnode = new Listnode(n);
+	newnode->next = head;
+	head = newnode;
+	top ++;
+}
+int LinkedListStack::pop() {
+	int temp_data = head->data;
+	Listnode *temp = head;
+	head = head->next;
+	top --;
+	delete temp;
+	return temp_data;
+}
+int LinkedListStack::get_size() {
+	return top + 1;
+}
+int main() {
+	/* Add test code */
+	return 0;
+}
