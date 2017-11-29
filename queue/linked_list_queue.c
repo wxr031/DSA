f45aa568bdd84f96c2b9132b909f58fdf77a3bf0
wxr031 <wxr031@gmail.com>
2017-11-29 23:12:47 +0800

    add

---
+++ queue/linked_list_queue.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+typedef struct listnode {
+	int data;
+	struct listnode *next;
+} Listnode;
+typedef struct queue {
+	int size;
+	Listnode *front, *rear;
+} Queue;
+void init_queue(Queue *q) {
+	q->size = 0;
+	q->front = q->rear = NULL;
+}
+int is_empty(Queue *q) {
+	return q->front == NULL;
+}
+void enqueue(Queue *q, int data) {
+	Listnode *new = (Listnode *)malloc(sizeof(Listnode));
+	new->data = data;
+	new->next = NULL;
+	q->size ++;
+	if (q->front == NULL) {
+		q->front = q->rear = new;
+		return;
+	}
+	q->rear->next = new;
+	q->rear = new;
+}
+int dequeue(Queue *q) {
+	if (is_empty(q)) {
+		printf("Underflow\n");
+		return INT_MIN;
+	}
+	Listnode *temp = q->front;
+	int return_data = temp->data;
+	q->front = q->front->next;
+	q->size --;
+	free(temp);
+	return return_data;
+}
+int main() {
+	/* Add test code */
+	return 0;
+}
