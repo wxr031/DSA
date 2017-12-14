6524fc92f36b17a501dc31a70993ca9b4f0644e9
wxr031 <wxr031@gmail.com>
2017-12-14 23:46:12 +0800

    need checking

--- tree/balanced_tree/AVL.c
+++ tree/balanced_tree/AVL.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 typedef struct treenode {
 	int data;
 	struct treenode *left, *right;
@@ -10,22 +11,18 @@ int max(int a, int b) {
 }
 int get_height(Treenode *root) {
 	if (!root) return -1;
-	else return root->height;
+	return root->height;
 }
 Treenode *LLrotation(Treenode *root) {
 	Treenode *temp = root->left;
 	root->left = temp->right;
 	temp->right = root;
-	root->height = max(get_height(root->left), get_hight(root->right)) + 1;
-	height->height = max(get_height(height->left), get_hight(height->right)) + 1;
 	return temp;
 }
 Treenode *RRrotation(Treenode *root) {
 	Treenode *temp = root->right;
-	root->left = temp->left;
+	root->right = temp->left;
 	temp->left = root;
-	root->height = max(get_height(root->left), get_hight(root->right)) + 1;
-	height->height = max(get_height(height->left), get_hight(height->right)) + 1;
 	return temp;
 }
 Treenode *LRrotation(Treenode *root) {
@@ -37,14 +34,96 @@ Treenode *RLrotation(Treenode *root) {
 	return RRrotation(root);
 }
 Treenode *insert(Treenode *root, int data) {
-	if (root->data == data) {
-		/* Add code */
+	if (!root) {
+		Treenode *newnode = (Treenode *)malloc(sizeof(Treenode));
+		assert(newnode);
+		newnode->data = data;
+		newnode->left = newnode->right = NULL;
+		newnode->height = 0;
+		return newnode;
 	}
-	else if (data < root->data)
+	if (data < root->data) {
 		root->left = insert(root->left, data);
-	else
+		if (get_height(root->left) > get_height(root->right) + 1) {
+			if (data < root->left->data)
+				root = LLrotation(root);
+			else
+				root = LRrotation(root);
+		}
+	}
+	else if (data > root->data) {
 		root->right = insert(root->right, data);
-	root->height = max(height(root->left), height(root->right)) + 1;
+		if (get_height(root->right) > get_height(root->left) + 1) {
+			if (data > root->right->data)
+				root = RRrotation(root);
+			else
+				root = RLrotation(root);
+		}
+	}
+	root->height = max(get_height(root->left), get_height(root->right)) + 1;
 	return root;
 }
-
+Treenode *delete(Treenode *root, int data) {
+	if (!root) return root;
+	if (root->data == data) {
+		if (!root->left || !root->right) {
+			Treenode *temp = (root->left)? root->left: root->right;
+			free(root);
+			return temp;
+		}
+		else {
+			Treenode *temp = root->left;
+			while (temp->right)
+				temp = temp->right;
+			root->data = temp->data;
+			root->left = delete(root->left, root->data);
+		}
+	}
+	if (data < root->data)
+		root->left = delete(root->left, data);
+	else if (data > root->data)
+		root->right = delete(root->right, data);
+	if (get_height(root->left) > get_height(root->right) + 1) {
+		if (get_height(root->left->left) >= get_height(root->left->right))
+			root = LLrotation(root);
+		else
+			root = LRrotation(root);
+	}
+	if (get_height(root->right) > get_height(root->left) + 1) {
+		if (get_height(root->right->right) >= get_height(root->right->left))
+			root = RRrotation(root);
+		else 
+			root = RLrotation(root);
+	}
+	root->height = max(get_height(root->left), get_height(root->right)) + 1;
+	return root;
+}
+void inorder(Treenode *root) {
+	if (!root) return;
+	inorder(root->left);
+	printf("%d\n", root->data);
+	inorder(root->right);
+}
+void preorder(Treenode *root) {
+	if (!root) return;
+	printf("%d\n", root->data);
+	preorder(root->left);
+	preorder(root->right);
+}
+int main() {
+	Treenode *root = NULL;
+	root = insert(root, 3);
+	root = insert(root, 2);
+	root = insert(root, 4);
+	root = insert(root, 5);
+	root = insert(root, 1);
+	root = insert(root, 6);
+	root = insert(root, 7);
+	root = delete(root, 1);
+	root = delete(root, 3);
+//	root = delete(root, 5);
+	inorder(root);
+	printf("----------------\n");
+	preorder(root);
+	return 0;
+}
