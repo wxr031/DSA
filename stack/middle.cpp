bb443756918251a24d9fe43fe5b0e212d729430b
wxr031 <wxr031@gmail.com>
2017-12-27 20:38:41 +0800

    add middle.cpp

--- stack/middle.cpp
+++ stack/middle.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
+struct listnode {
+	int data;
+	struct listnode *left, *right;
+	listnode(int D): data(D) {
+		left = right = nullptr;
+	}
+};
+class DLL {
+	struct listnode *head, *dummy_head, *dummy_tail, *mid;
+public:
+	DLL() {
+		head = dummy_head = dummy_tail = mid = nullptr;
+	}
+	void insert_head(int D) {
+		struct listnode *new_head = new struct listnode(D);
+		new_head->right = head;
+		new_head->left = dummy_head;
+		dummy_head->right = new_head;
+		head->left = new_head;
+		head = new_head;
+	}
+	friend class stack;
+
+#ifdef DEBUG
+	friend void debug();
+#endif
+
+};
 class stack {
-	struct listnode *head, *mid;
+	DLL list;
 	bool even; 
 public:
 	stack(): even(true) {
-		struct listnode dummy_head(INT_MIN);
-		struct listnode dummy_tail(INT_MIN);
-		new_head->right = dummy_tail;
-		dummy_tail->left = dummy_head;
-		mid = dummy_head;
-		head = dummy_tail;
+		list.dummy_head = new struct listnode(INT_MIN);
+		list.dummy_tail = new struct listnode(INT_MIN);
+		list.dummy_head->right = list.dummy_tail;
+		list.dummy_tail->left = list.dummy_head;
+		list.mid = list.dummy_head;
+		list.head = list.dummy_tail;
 	}
 	void push(int);
 	int pop();
@@ -17,51 +45,43 @@ public:
 	void delete_middle();
 };
 void stack::push(int data) {
-	head.insert_head(data);
-	if (even) mid = mid->right;
-	else mid = mid->left;;
+	list.insert_head(data);
+	if (list.mid->data == INT_MIN) list.mid = list.head;
+	else if (!even) list.mid = list.mid->left;
 	even = !even;
 }
 int stack::pop() {
-	struct listnode *temp = head;
+	struct listnode *temp = list.head;
 	int ret = temp->data;
-	if (even) mid = mid->right;
-	else mid = mid->left;
-	even = !even;
-	head = head->right;
-	head->left = temp->left;
-	head->left->right = head;
+	list.head = list.head->right;
+	list.head->left = temp->left;
+	list.head->left->right = list.head;
 	delete temp;
+	if (even) list.mid = list.mid->right;
+	else if (list.head->data == INT_MIN) list.mid = list.head->left;
+	even = !even;
 	return ret;
 }
 int stack::find_middle() {
-	return mid->data;
+	return list.mid->data;
 }
-void stack::delete_min() {
-	struct listnode *temp = mid;
+void stack::delete_middle() {
+	struct listnode *temp = list.mid;
 	if (even) {
-		mid = mid->right;
-		mid->left = temp->left;
-		mid->left->right = mid;
+		list.mid = list.mid->right;
+		list.mid->left = temp->left;
+		list.mid->left->right = list.mid;
 	}
 	else {
-		mid = mid->left;
-		mid->right = temp->right;
-		temp->right->left = mid;
+		list.mid = list.mid->left;
+		list.mid->right = temp->right;
+		temp->right->left = list.mid;
 	}
 	even = !even;
+	if (list.mid->data == INT_MIN) list.head = list.mid->right;
 	delete temp;
 }
 int main() {
-	stack S;
-	S.push(11);
-	S.push(22);
-	S.push(33);
-	S.push(44);
-	S.push(55);
-	S.push(66);
-	S.push(77);
-	std::cout << S.pop() << std::endl;
-	std::cout << S.pop() << std::endl;
-	std::cout << S.pop() << std::endl;
+	/* Add test code */
+	return 0;
 }
